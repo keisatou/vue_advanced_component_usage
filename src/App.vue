407531d09e0018f25d56c38aa083bb928a47cdf7
keisatou <dummy>
2017-04-06 22:23:09 +0900

    pass quote as props

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-
+        <app-quote quote="A wonderful quote!"></app-quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Quote from './components/Quote.vue';
 
 export default {
-
-}
+  components: {
+    'app-quote': Quote,
+  },
+};
 </script>
 
 <style>
