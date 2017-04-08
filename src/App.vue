78f6ff17f9c49de9c3ccd7e501a2150ddf6fa5b9
keisatou <dummy>
2017-04-08 22:24:24 +0900

    keep Components alive

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,11 @@
         <button @click="selectedComponent = 'app-new'">New</button>
         <hr>
         <p>{{ selectedComponent }}</p>
-        <component :is="selectedComponent">
-          <p>Default Component</p>
-        </component>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>Default Component</p>
+          </component>
+        </keep-alive>
         <!-- <app-quote>
           <h2 slot="title">{{ quoteTitle }}</h2>
           <p slot="content">A wonderful Quote</p>
