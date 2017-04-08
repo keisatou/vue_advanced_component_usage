a8a63ac90ff815eca39a9e0f64e262c07c373fbe
keisatou <dummy>
2017-04-08 22:20:38 +0900

    Dynamic Components are destroyed

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h3>The Quote</h3>
+    <button @click="counter++">Increase!</button>
+    {{ counter }}
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  destroyed() {
+    console.log('New.vue Destroyed!');
+  },
 };
 </script>
 
